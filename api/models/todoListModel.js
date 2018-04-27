ba897e5a30bb9aacae2fac1b8992cfb31302fa86
shariatululoom <658690@cognizant.com>
2018-04-27 18:11:52 +0530

    first deploy

---
+++ api/models/todoListModel.js
@@ -0,0 +1,37 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+var TaskSchema2 = new Schema({
+  "name": {
+    type: String,
+  
+  },
+ 
+ "experience": {
+    type: String,
+    
+  },
+   
+  }
+});
+module.exports = mongoose.model('Tasks', TaskSchema);
+module.exports = mongoose.model('db', TaskSchema2);
